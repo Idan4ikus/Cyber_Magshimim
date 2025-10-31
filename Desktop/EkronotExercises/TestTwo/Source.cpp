55f24609f45e97df36328e4eb4c5372b55100069
Idan <idan4ikus@gmail.com>
2025-10-31 18:10:30 +0200

    Git test

--- Desktop/EkronotExercises/TestTwo/Source.cpp
+++ Desktop/EkronotExercises/TestTwo/Source.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "Queue.h"
-#include "smth.h"
 
 
 void initQueue(Queue* q, unsigned int size)
@@ -11,4 +10,45 @@ void initQueue(Queue* q, unsigned int size)
 	q->size = 0;
 	q->capacity = size;
 
+}
+
+
+void cleanQueue(Queue* q)
+{
+	q->data = nullptr;
+	q->front = 0;
+	q->rear = 0;
+	q->size = 0;
+	q->capacity = 0;
+}
+
+
+void enqueue(Queue* q, unsigned int newValue)
+{
+	q->data[q->rear] = newValue;
+	q->rear = q->rear + 1;
+	q->size++;
+}
+
+int dequeue(Queue* q)
+{
+	unsigned int num = q->data[q->front];
+	q->front = (q->front + +1) % q->capacity;
+	q->size--;
+
+	return num;
+}
+
+bool isEmpty(Queue* s)
+{
+	if (s->size == 0)
+		return true;
+	return false;
+}
+
+bool isFull(Queue* s)
+{
+	if (s->size == s->capacity)
+		return true;
+	return false;
 }
